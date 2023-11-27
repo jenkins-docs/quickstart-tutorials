bf921411dac477bb1297c35ff55d2b9a26938279
gounthar <gounthar@gmail.com>
2023-11-27 10:30:27 +0100

    fix(gitpod): Displaying targets.

--- dockerfiles/gitpodURL.sh
+++ dockerfiles/gitpodURL.sh
@@ -8,6 +8,7 @@ service_url=$(echo "$GITPOD_WORKSPACE_URL" | awk -F/ '{print $3}')
 
 # Print the hostname for debugging purposes
 echo -e "Once you enter \033[42;30mdocker compose up _target_\033[0m, Jenkins will be accessible here: \033[36mhttps://8080-$service_url\033[0m"
+echo -e "As a gentle reminder, the current targets are: \033[36mmaven\033[0m, \033[36mnode\033[0m, \033[36mpython\033[0m, and \033[36mmulti\033[0m"
 
 # Use yq to update the value of the .unclassified.location.url field in the configuration file
 yq eval ".unclassified.location.url = \"https://8080-$service_url/\"" "$config_file" > "$config_file.tmp" && mv "$config_file.tmp" "$config_file"
