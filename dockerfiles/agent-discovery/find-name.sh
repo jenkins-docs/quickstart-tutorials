38a9e4b4ecbf7786be25100e56d9847705b405c6
Bruno Verachten <gounthar@gmail.com>
2024-04-01 12:52:03 +0200

    fix(shell): ShellCheck detected problems.

--- dockerfiles/agent-discovery/find-name.sh
+++ dockerfiles/agent-discovery/find-name.sh
@@ -10,28 +10,27 @@ IP_ADDRESS=$(hostname -I | awk '{print $1}')
 
 # Get the first three octets of the IP address
 # The cut command is used to split the IP address by '.' and print the first three fields.
-THREE_OCTETS=$(echo $IP_ADDRESS | cut -d '.' -f 1-3)
+THREE_OCTETS=$(echo "$IP_ADDRESS" | cut -d '.' -f 1-3)
 
 # Run the nmap command with the appropriate subnet mask and print the output
 # The script enters an infinite loop where it continuously scans the network for hosts.
 while true; do
   # The nmap -sn command is used to perform a ping scan of the network.
   # The $THREE_OCTETS.0/24 argument specifies the subnet mask for the network scan.
-  NMAP_OUTPUT=$(nmap -sn $THREE_OCTETS.0/24)
+  NMAP_OUTPUT=$(nmap -sn "$THREE_OCTETS".0/24)
   echo "nmap output: $NMAP_OUTPUT"
 
   # Split the nmap output into lines
-  IFS=$'\n' NMAP_LINES=($NMAP_OUTPUT)
-
+  IFS=$'\n' NMAP_LINES=("$NMAP_OUTPUT")
   # Search for the line containing "agent"
   for LINE in "${NMAP_LINES[@]}"; do
     if [[ $LINE == *"agent"* ]]; then
       MATCHING_HOST=$LINE
       echo "$MATCHING_HOST"
       # The awk command is used to print the 5th field of the matching line, which should be the machine name.
-      MACHINE_NAME=$(echo $MATCHING_HOST | awk '{print $5}')
+      MACHINE_NAME=$(echo "$MATCHING_HOST" | awk '{print $5}')
       # Split the machine name by "." and print the first field
-      MACHINE_NAME=$(echo $MACHINE_NAME | cut -d '.' -f 1)
+      MACHINE_NAME=$(echo "$MACHINE_NAME" | cut -d '.' -f 1)
       echo "Machine name: $MACHINE_NAME"
 
       # Export the MACHINE_NAME variable to the environment
